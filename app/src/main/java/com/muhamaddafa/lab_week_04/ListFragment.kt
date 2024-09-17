46f088818d82af4d0d6c8ab6d93df25785cb5d8e
muhamaddafa <gaming102dafa@gmail.com>
2024-09-17 10:28:58 +0700

    update<DetailFrgament, ListFragment>: update code

--- app/src/main/java/com/muhamaddafa/lab_week_04/ListFragment.kt
+++ app/src/main/java/com/muhamaddafa/lab_week_04/ListFragment.kt
@@ -17,43 +17,36 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class ListFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
+    class ListFragment : Fragment() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
         }
-    }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_list, container, false)
-    }
+        override fun onCreateView(
+            inflater: LayoutInflater, container: ViewGroup?,
+            savedInstanceState: Bundle?
+        ): View? {
+// Inflate the layout for this fragment
+            return inflater.inflate(R.layout.fragment_list, container, false)
+        }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ListFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ListFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+        override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+            super.onViewCreated(view, savedInstanceState)
+            val coffeeList = listOf<View>(
+                view.findViewById(R.id.affogato),
+                view.findViewById(R.id.americano),
+                view.findViewById(R.id.latte)
+            )
+            coffeeList.forEach { coffee ->
+                val fragmentBundle = Bundle()
+                fragmentBundle.putInt(COFFEE_ID, coffee.id)
+                coffee.setOnClickListener(
+                    Navigation.createNavigateOnClickListener(
+                        R.id.action_listFragment_to_detailFragment,
+                        fragmentBundle
+                    )
+                )
             }
+        }
     }
 }
