2c611fa87a2c1d5b6f96246707ee4162cbb37da7
muhamaddafa <gaming102dafa@gmail.com>
2024-09-17 17:42:58 +0700

    final: assignment 4

--- app/src/main/java/CafeAdapter.kt
+++ app/src/main/java/CafeAdapter.kt
@@ -1,3 +1,4 @@
+import android.content.Context
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.lifecycle.Lifecycle
@@ -10,13 +11,20 @@ val TABS_FIXED = listOf(
     R.string.janjijiwa_title,
     R.string.kopikenangan_title,
 )
-class CafeAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
-    FragmentStateAdapter(fragmentManager, lifecycle) {
+
+val TABS_CONTENT_FIXED = listOf(
+    R.string.starbucks_desc,
+    R.string.janjijiwa_desc,
+    R.string.kopikenangan_desc,
+)
+
+class CafeAdapter(private val context: Context, fragmentManager: FragmentManager, lifecycle: Lifecycle) : FragmentStateAdapter(fragmentManager, lifecycle) {
+
     override fun getItemCount(): Int {
         return TABS_FIXED.size
     }
     override fun createFragment(position: Int): Fragment
     {
-        return CafeDetailFragment()
+        return CafeDetailFragment.newInstance(context.getString(TABS_CONTENT_FIXED[position]))
     }
 }
--- app/src/main/java/com/muhamaddafa/lab_week_04/CafeFragment.kt
+++ app/src/main/java/com/muhamaddafa/lab_week_04/CafeFragment.kt
@@ -12,21 +12,24 @@ import com.google.android.material.tabs.TabLayout
 import com.google.android.material.tabs.TabLayoutMediator
 
 class CafeFragment : Fragment() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-// Inflate the layout for this fragment
+        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_cafe, container, false)
     }
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val viewPager = view.findViewById<ViewPager2>(R.id.view_pager)
         val tabLayout = view.findViewById<TabLayout>(R.id.tab_layout)
-        val adapter = CafeAdapter(childFragmentManager, lifecycle)
+        val adapter = CafeAdapter(requireContext(), childFragmentManager, lifecycle)
         viewPager.adapter = adapter
         TabLayoutMediator(tabLayout, viewPager) { tab, position ->
             tab.text = resources.getString(TABS_FIXED[position])
